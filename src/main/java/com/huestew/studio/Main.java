333b6a749fd441f7456d39373f2112fb403f3fe3
Patrik Olson <patrik.olson@live.com>
2016-04-18 19:54:26 +0200

    Added package and Main class

---
+++ src/main/java/com/huestew/studio/Main.java
@@ -0,0 +1,15 @@
+package com.huestew.studio;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
