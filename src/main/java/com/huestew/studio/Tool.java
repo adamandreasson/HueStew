f7312f8cf5943ae8c63964b4ff05458ae729192e
Daniel Illipe <dannne.i@gmail.com>
2016-04-19 15:32:51 +0200

    Add Tool and Toolbox

--- src/main/java/com/huestew/studio/Tool.java
+++ src/main/java/com/huestew/studio/Tool.java
@@ -1,5 +1,35 @@
 package com.huestew.studio;
 
+
+import javafx.event.Event;
+
+
+
+/**
+ * 
+ * A tool that manipulates lighttracks.
+ * 
+ * @author Daniel Illipe
+ * @author Patrik Olson
+ * @author Marcus Randevik
+ * @author Adam Andreasson
+ * 
+ */
+
+
 public interface Tool {
+	
+	
+	/**
+	 * 
+	 * Manipulates a lighttrack depending on the event.
+	 * 
+	 * @param event The event to be performed.
+	 * @param lighttrack The lighttrack that is being manipulated.
+	 * @param timestamp At which time the event should occur on the lighttrack.
+	 *
+	 */
+	void doAction(Event event, LightTrack lighttrack, timestamp timestamp);
+		
 
 }
--- src/main/java/com/huestew/studio/Toolbox.java
+++ src/main/java/com/huestew/studio/Toolbox.java
@@ -1,5 +1,41 @@
 package com.huestew.studio;
 
+import java.util.List;
+
+
+
+/**
+ * 
+ * A toolbox containing a list of tools.
+ * 
+ * 
+ * @author Daniel Illipe
+ * @author Patrik Olson
+ * @author Marcus Randevik
+ * @author Adam Andreasson
+ * 
+ */
+
 public class Toolbox {
+	
+	
+	private List<Tool> tools;
+	
+	private Tool tool;
+	
+	
+	/**
+	 * 
+	 * @return returns current tool
+	 * 
+	 */
+	public Tool getTool(){
+		
+		return tool;
+		
+	}
+	
+	
+	
 
 }
