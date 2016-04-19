f11e9bf7c65e37b6e455d27a5c9811967df98f96
Adam Andreasson <hej@adamandreasson.se>
2016-04-19 20:25:10 +0200

    More JavaFX things, added icon in terrible location pls fix

--- src/main/java/com/huestew/studio/Main.java
+++ src/main/java/com/huestew/studio/Main.java
@@ -2,21 +2,24 @@ package com.huestew.studio;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		MainViewController controller = (MainViewController) Util.loadFxml("main.fxml");
-        primaryStage.setTitle("HueStew Studio");
-        Scene scene = new Scene(controller.getView());
-        primaryStage.setScene(scene);
-        
-        primaryStage.sizeToScene();
-        primaryStage.show();
+		primaryStage.setTitle("HueStew Studio");
+		Scene scene = new Scene(controller.getView());
+		primaryStage.setScene(scene);
+
+		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon_16x16.png")));
+		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon_256x256.png")));
+		primaryStage.sizeToScene();
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
