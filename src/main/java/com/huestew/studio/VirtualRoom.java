505796c76db7dfc0efa258b034da1b93826b3707
Marcus Randevik <marcus.randevik@hotmail.se>
2016-04-20 07:46:00 +0200

    Added commets for the virtualroom class

--- src/main/java/com/huestew/studio/VirtualRoom.java
+++ src/main/java/com/huestew/studio/VirtualRoom.java
@@ -7,24 +7,50 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * A class for graphical representation of virtual bulbs. 
+ * @author Adam
+ *
+ */
 public class VirtualRoom {
 
+	/** the canvas used to draw the virtual room in.**/
 	private Canvas canvas;
+	
+	/** list of all virtual bulbs in the virtual room **/
 	private List<VirtualBulb> bulbs;
 
+	/**
+	 * Create a new virtual room with an associated canvas
+	 * @param canvas
+	 * 				the canvas in which the virtual room will be drawn.
+	 */
 	public VirtualRoom(Canvas canvas) {
 		this.canvas = canvas;
 		bulbs = new ArrayList<VirtualBulb>();
 	}
-
+	
+	/**
+	 * Adds a bulb to the list
+	 * @param bulb
+	 * 				the buld which is to be added to the list
+	 */
 	public void addBulb(VirtualBulb bulb) {
 		bulbs.add(bulb);
 	}
 
+	/**
+	 * Sets a new canvas for the virtual room to be drawn in.
+	 * @param canvas
+	 * 				the new canvas.
+	 */				
 	public void setCanvas(Canvas canvas) {
 		this.canvas = canvas;
 	}
 
+	/**
+	 * Redraws the entire virtual room including the bulbs.
+	 */
 	public void redraw() {
 
 		GraphicsContext gc = canvas.getGraphicsContext2D();
