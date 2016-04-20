0d261393241a97701a65124fbb6d5ec8ccbeebf3
Marcus Randevik <marcus.randevik@hotmail.se>
2016-04-20 08:02:40 +0200

    Added method head for player

--- src/main/java/com/huestew/studio/Player.java
+++ src/main/java/com/huestew/studio/Player.java
@@ -1,7 +1,32 @@
 package com.huestew.studio;
 
+/**
+ * A class for playing a lightshow.
+ * @author Marcus
+ *
+ */
 public class Player {
 
+	/** used to determine whether or not the player is currently playing a show **/
+	private boolean playing;
 	
+	/** starts playing the show **/
+	public void play() {
+		//TODO
+	}
 	
+	/** pauses the show at the current timestamp **/
+	public void pause() {
+		//TODO
+	}
+	
+	/** stops playing the show and resets the timestamp to 0 **/
+	public void stop() {
+		//TODO
+	}
+	
+	/** Determines whether or not the player is currently playing **/
+	public boolean isPlaying() {
+		return playing;
+	}
 }
