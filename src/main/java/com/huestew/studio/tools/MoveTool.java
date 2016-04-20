0f4c6d8f57e1a18200cfdbe295f544bb66532ac2
Marcus Randevik <marcus.randevik@hotmail.se>
2016-04-20 21:28:58 +0200

    New tool classes added

---
+++ src/main/java/com/huestew/studio/tools/MoveTool.java
@@ -0,0 +1,16 @@
+package com.huestew.studio.tools;
+
+import com.huestew.studio.LightTrack;
+import com.huestew.studio.Tool;
+
+import javafx.event.Event;
+
+public class MoveTool implements Tool {
+
+	@Override
+	public void doAction(Event event, LightTrack lightTrack, int timestamp) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
