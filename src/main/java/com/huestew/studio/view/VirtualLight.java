592d7caee977e57040076d964d5ab9c8e8f09fa5
Marcus Randevik <marcus.randevik@hotmail.se>
2016-04-25 09:29:05 +0200

    Virtuallight now also has some comments

--- src/main/java/com/huestew/studio/view/VirtualLight.java
+++ src/main/java/com/huestew/studio/view/VirtualLight.java
@@ -14,17 +14,33 @@ import com.huestew.studio.model.VirtualBulb;
  */
 public class VirtualLight implements Light {
 
+	/** the virtual bulb assigned to this controller. **/
 	private VirtualBulb bulb;
 
+	/**
+	 * Creates a new virtualLight with a bulb assigned to it.
+	 * @param bulb
+	 * 				the bulb which is assigned to the new virtualight.
+	 */
 	public VirtualLight(VirtualBulb bulb) {
 		this.bulb = bulb;
 	}
 	
+	/**
+	 * Get the bulb assigned to this virtuallight
+	 * @return
+	 * 			the bulb assigned to this virutallight
+	 */
 	public VirtualBulb getBulb(){
 		return bulb;
 	}
 	
 	@Override
+	/**
+	 * Send a command to the assigned virtualbulb to change its state
+	 * @param state
+	 * 				the new state for the virtualbulb
+	 */
 	public void setState(LightState state) {
 		bulb.setColor(state.getColor());
 	}
