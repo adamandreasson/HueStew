764dde4f4f0ae02c83a0f695b0ea6ee117eae2e2
Adam Andreasson <hej@adamandreasson.se>
2016-05-11 13:58:22 +0200

    New remove tool cursor

--- src/main/java/com/huestew/studio/tools/RemoveTool.java
+++ src/main/java/com/huestew/studio/tools/RemoveTool.java
@@ -4,10 +4,15 @@ import com.huestew.studio.model.KeyFrame;
 import com.huestew.studio.model.LightTrack;
 
 import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class RemoveTool implements Tool {
+
+	private Cursor cursor = new ImageCursor(new Image("cursor_remove.png"), 0, 0);
+	
 	@Override
 	public void doAction(MouseEvent event, LightTrack lightTrack, KeyFrame keyFrame, int timestamp, double normalizedY) {
 		if (event.getEventType() == MouseEvent.MOUSE_CLICKED && keyFrame != null) {
@@ -22,6 +27,6 @@ public class RemoveTool implements Tool {
 
 	@Override
 	public Cursor getCursor(boolean hoveringKeyFrame, boolean isMouseDown) {
-		return Cursor.CROSSHAIR;
+		return cursor;
 	}
 }
