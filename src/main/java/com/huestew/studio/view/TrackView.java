17f9231bc20ff092d7760fd496aa62b200bbadf2
Adam Andreasson <hej@adamandreasson.se>
2016-05-11 15:19:07 +0200

    Fixed scrollpane height, trackview pixels per second because javafx limitatations

--- src/main/java/com/huestew/studio/view/TrackView.java
+++ src/main/java/com/huestew/studio/view/TrackView.java
@@ -32,7 +32,7 @@ import javafx.stage.WindowEvent;
  */
 public class TrackView {
 	private static final int KEY_FRAME_SIZE = 5;
-	public static final int PIXELS_PER_SECOND = 100;
+	public static final int PIXELS_PER_SECOND = 10;
 
 	private Canvas canvas;
 	private String backgroundWaveFilePath = null;
