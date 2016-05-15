64ab89880bd1a7235f87b71441c07b97b92a4f7b
Patrik Olson <patrik.olson@live.com>
2016-05-16 01:39:55 +0200

    Draw virtual room on fx thread

--- src/main/java/com/huestew/studio/view/VirtualRoom.java
+++ src/main/java/com/huestew/studio/view/VirtualRoom.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.huestew.studio.model.VirtualBulb;
 
+import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -61,24 +62,27 @@ public class VirtualRoom {
 		if (canvas == null)
 			return;
 
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		gc.setFill(Color.BLACK);
-		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		Platform.runLater(new Runnable() {
+			public void run() {
+				GraphicsContext gc = canvas.getGraphicsContext2D();
+				gc.setFill(Color.BLACK);
+				gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-		for (VirtualBulb bulb : bulbs) {
-			
-			double bulbSize = canvas.getWidth()/6;
+				for (VirtualBulb bulb : bulbs) {
+					
+					double bulbSize = canvas.getWidth()/6;
 
-			Color bulbColor = new Color(bulb.getState().getColor().getRed(), bulb.getState().getColor().getGreen(),
-					bulb.getState().getColor().getBlue(), (bulb.getState().getBrightness() / 255.0));
-			
-			double firstStop = 0.0 + 0.1 * (bulb.getState().getBrightness() / 255.0);
+					Color bulbColor = new Color(bulb.getState().getColor().getRed(), bulb.getState().getColor().getGreen(),
+							bulb.getState().getColor().getBlue(), (bulb.getState().getBrightness() / 255.0));
+					
+					double firstStop = 0.0 + 0.1 * (bulb.getState().getBrightness() / 255.0);
 
-			gc.setFill(new RadialGradient(0, 0, 0.5, 0.5, 0.5, true, CycleMethod.REFLECT,
-					new Stop(firstStop, bulbColor), new Stop(1.0, Color.BLACK)));
-			gc.fillOval(bulb.getX() * canvas.getWidth() - bulbSize/2, bulb.getY() * canvas.getHeight() - bulbSize/2, bulbSize, bulbSize);
-		}
-
+					gc.setFill(new RadialGradient(0, 0, 0.5, 0.5, 0.5, true, CycleMethod.REFLECT,
+							new Stop(firstStop, bulbColor), new Stop(1.0, Color.BLACK)));
+					gc.fillOval(bulb.getX() * canvas.getWidth() - bulbSize/2, bulb.getY() * canvas.getHeight() - bulbSize/2, bulbSize, bulbSize);
+				}
+			}
+		});
 	}
 
 }
