39a5cf54b6482f9284d0535a5b5c1c548dad3aaf
Marcus Randevik <marcus.randevik@hotmail.se>
2016-05-15 14:06:30 +0200

    Made changes to the custom color class so that you no longer are able to set illegal values, hashcode is no corrext and some javadocs.

--- src/main/java/com/huestew/studio/model/Color.java
+++ src/main/java/com/huestew/studio/model/Color.java
@@ -4,6 +4,9 @@
 package com.huestew.studio.model;
 
 /**
+ * A custom class for storing color.
+ * Stores the values based on the RGB scale with minimum value 0 and 
+ * maximum value 1 for each of the colors.
  * @author Adam
  *
  */
@@ -13,24 +16,48 @@ public class Color {
 	private double green;
 	private double blue;
 	
+	/**
+	 * Creates a new color object with the specified values.
+	 * @param red
+	 * 				the amount of red.
+	 * @param green
+	 * 				the amount of green.
+	 * @param blue
+	 * 				the amount of blue.
+	 */
 	public Color(double red, double green, double blue){
-		this.red = red;
-		this.green = green;
-		this.blue = blue;
+		setRed(red);
+		setGreen(green);
+		setBlue(blue);
 	}
 	
+	/**
+	 * Creates a new color object based on the values of another.
+	 * @param color
+	 * 				the color to be copied.
+	 */
 	public Color(Color color){
 		this.red = color.getRed();
 		this.green = color.getGreen();
 		this.blue = color.getBlue();
 	}
 	
+	/**
+	 * Creates a new color object based on the values of a JavaFX color object.
+	 * @param color
+	 * 				the JavaFX color object to be copied.
+	 */
 	public Color(javafx.scene.paint.Color color){
 		this.red = color.getRed();
 		this.green = color.getGreen();
 		this.blue = color.getBlue();
 	}
 	
+	/**
+	 * Converts the color to a JavaFX color with the opacity 1.
+	 * @return
+	 * 			the color as a JavaFx color
+	 */
 	public javafx.scene.paint.Color toFxColor(){
 		return new javafx.scene.paint.Color(red, green, blue, 1);
 	}
@@ -41,35 +68,52 @@ public class Color {
 	public double getRed() {
 		return red;
 	}
+	
 	/**
 	 * @param red the red to set
 	 */
 	public void setRed(double red) {
-		this.red = red;
+		if (red < 0 || red > 255) {
+			this.red = red;
+		} else {
+			throw new IllegalArgumentException("Red must be between 0-255");
+		}		
 	}
+	
 	/**
 	 * @return the green
 	 */
 	public double getGreen() {
 		return green;
 	}
+	
 	/**
 	 * @param green the green to set
 	 */
 	public void setGreen(double green) {
-		this.green = green;
+		if (green < 0 || green > 255) {
+			this.green = green;	
+		} else {
+			throw new IllegalArgumentException("Green must be between 0-255");
+		}
 	}
+	
 	/**
 	 * @return the blue
 	 */
 	public double getBlue() {
 		return blue;
 	}
+	
 	/**
 	 * @param blue the blue to set
 	 */
 	public void setBlue(double blue) {
-		this.blue = blue;
+		if (blue < 0 || blue > 255) {
+			this.blue = blue;	
+		} else {
+			throw new IllegalArgumentException("Blue must be between 0-255");
+		}
 	}
 
 	@Override
@@ -84,10 +128,13 @@ public class Color {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int)red*73 + (int)green*13 + (int)blue*11;
+		result = prime * result + (int)(red*73) + (int)(green*13) + (int)(blue*11);
 		return result;
 	}
 
+	/**
+	 * Compares two color objects based on their RGB values.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
