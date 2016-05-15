7492a06d5f62cd9db2850c968fa6ba9c73119567
Patrik Olson <patrik.olson@live.com>
2016-05-15 14:39:33 +0200

    More tests for Show

--- src/test/java/com/huestew/studio/ShowTest.java
+++ src/test/java/com/huestew/studio/ShowTest.java
@@ -2,9 +2,12 @@ package com.huestew.studio;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.huestew.studio.model.Audio;
 import com.huestew.studio.model.LightTrack;
 import com.huestew.studio.model.Show;
 
@@ -39,5 +42,30 @@ public class ShowTest {
 		show.getLightTracks().add(new LightTrack());
 		
 		assertTrue(show.getLightTracks().size() == 0);
+	}
+	
+	@Test
+	public void setDuration() {
+		show.setDuration(500);
+		
+		assertTrue(show.getDuration() == 500);
+		
+		boolean negative = true;
+		try {
+			show.setDuration(-500);
+		} catch (IllegalArgumentException e) {
+			negative = false;
+		}
+		
+		assertFalse(negative);
+		assertTrue(show.getDuration() >= 0);
+	}
+	
+	@Test
+	public void setAudio() {
+		Audio audio = new Audio(new File(System.getProperty("user.home")).listFiles()[0]);
+		show.setAudio(audio);
+		
+		assertTrue(show.getAudio().equals(audio));
 	}
 }
