307ae82ccdf7d52b28d7a9b98aa527fa7edfbb60
Patrik Olson <patrik.olson@live.com>
2016-05-19 00:55:10 +0200

    Remove unused inner class

--- src/main/java/com/huestew/studio/util/WaveBuilder.java
+++ src/main/java/com/huestew/studio/util/WaveBuilder.java
@@ -39,7 +39,6 @@ public class WaveBuilder {
 
 	AudioInputStream audioInputStream;
 	Vector<Line2D.Double> lines = new Vector<Line2D.Double>();
-	Capture capture = new Capture();
 	double duration, seconds;
 	String fileName;
 	SamplingGraph samplingGraph;
@@ -237,131 +236,13 @@ public class WaveBuilder {
 		public void run() {
 			seconds = 0;
 			while (thread != null) {
-				if ((capture.line != null) && (capture.line.isActive())) {
-					long milliseconds = (long) (capture.line.getMicrosecondPosition() / 1000);
-					seconds = milliseconds / 1000.0;
-				}
 				try {
 					Thread.sleep(100);
 				} catch (Exception e) {
 					break;
 				}
-				while ((capture.line != null && !capture.line.isActive())) {
-					try {
-						Thread.sleep(10);
-					} catch (Exception e) {
-						break;
-					}
-				}
 			}
 			seconds = 0;
 		}
 	} // End class SamplingGraph
-
-	/**
-	 * Reads data from the input channel and writes to the output stream
-	 */
-	class Capture implements Runnable {
-
-		TargetDataLine line;
-		Thread thread;
-
-		public void start() {
-			thread = new Thread(this);
-			thread.setName("Capture");
-			thread.start();
-		}
-
-		public void stop() {
-			thread = null;
-		}
-
-		private void shutDown(String message) {
-			thread = null;
-			samplingGraph.stop();
-		}
-
-		public void run() {
-
-			duration = 0;
-			audioInputStream = null;
-
-			// define the required attributes for our line,
-			// and make sure a compatible line is supported.
-
-			AudioFormat format = audioInputStream.getFormat();
-			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-
-			if (!AudioSystem.isLineSupported(info)) {
-				shutDown("Line matching " + info + " not supported.");
-				return;
-			}
-
-			// get and open the target data line for capture.
-
-			try {
-				line = (TargetDataLine) AudioSystem.getLine(info);
-				line.open(format, line.getBufferSize());
-			} catch (LineUnavailableException ex) {
-				shutDown("Unable to open the line: " + ex);
-				return;
-			} catch (SecurityException ex) {
-				shutDown(ex.toString());
-				// JavaSound.showInfoDialog();
-				return;
-			} catch (Exception ex) {
-				shutDown(ex.toString());
-				return;
-			}
-
-			// play back the captured audio data
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			int frameSizeInBytes = format.getFrameSize();
-			int bufferLengthInFrames = line.getBufferSize() / 8;
-			int bufferLengthInBytes = bufferLengthInFrames * frameSizeInBytes;
-			byte[] data = new byte[bufferLengthInBytes];
-			int numBytesRead;
-
-			line.start();
-
-			while (thread != null) {
-				if ((numBytesRead = line.read(data, 0, bufferLengthInBytes)) == -1) {
-					break;
-				}
-				out.write(data, 0, numBytesRead);
-			}
-
-			// we reached the end of the stream. stop and close the line.
-			line.stop();
-			line.close();
-			line = null;
-
-			// stop and close the output stream
-			try {
-				out.flush();
-				out.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-
-			// load bytes into the audio input stream for playback
-
-			byte audioBytes[] = out.toByteArray();
-			ByteArrayInputStream bais = new ByteArrayInputStream(audioBytes);
-			audioInputStream = new AudioInputStream(bais, format, audioBytes.length / frameSizeInBytes);
-
-			long milliseconds = (long) ((audioInputStream.getFrameLength() * 1000) / format.getFrameRate());
-			duration = milliseconds / 1000.0;
-
-			try {
-				audioInputStream.reset();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				return;
-			}
-
-			samplingGraph.createWaveForm(audioBytes);
-		}
-	} // End class Capture
-
 }
