aacabd1a874ade3e5d229996d651215fe35020c5
Adam Andreasson <hej@adamandreasson.se>
2016-05-20 17:05:59 +0200

    Proper formatting in SelewctTool

--- src/main/java/com/huestew/studio/controller/tools/SelectTool.java
+++ src/main/java/com/huestew/studio/controller/tools/SelectTool.java
@@ -1,18 +1,15 @@
 package com.huestew.studio.controller.tools;
 
-
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-
 import com.huestew.studio.HueStew;
 import com.huestew.studio.model.KeyFrame;
 import com.huestew.studio.model.LightState;
 import com.huestew.studio.model.LightTrack;
-
 
 import javafx.scene.Cursor;
 import javafx.scene.input.KeyEvent;
@@ -33,65 +30,62 @@ public class SelectTool extends Tool {
 	private int ceiling;
 	private boolean moveHorizontally = true;
 	private boolean moveVertically = true;
-    private Set<KeyFrame> withoutSelectedKeyFrame;
-  
+	private Set<KeyFrame> withoutSelectedKeyFrame;
 
 	public SelectTool(Toolbox toolbox) {
 		super(toolbox);
 	}
 
-	private void deleteSelectedKeyFrames(){
+	private void deleteSelectedKeyFrames() {
 		if (!selectedKeyFrames.isEmpty()) {
 			Iterator<KeyFrame> deleter = selectedKeyFrames.iterator();
-			
-			while(deleter.hasNext()){
+
+			while (deleter.hasNext()) {
 				KeyFrame temp = deleter.next();
 				temp.remove();
 				HueStew.getInstance().getView().updateTrackView();
 			}
-			//selectedLightTrack.removeKeyFrame(selectedKeyFrame);
-			//selectedKeyFrame = null;
+			// selectedLightTrack.removeKeyFrame(selectedKeyFrame);
+			// selectedKeyFrame = null;
 		}
 	}
-	
-	@Override
-	public void doAction(MouseEvent event, LightTrack lightTrack, KeyFrame keyFrame, Set<KeyFrame> keyFramesSelected, int timestamp, double normalizedY) {
-			// Store selected key frame/s and light track
-			selectedKeyFrames = keyFramesSelected;
-			selectedKeyFrame = keyFrame;
-			selectedLightTrack = lightTrack;
-		
-		if (event.getEventType() == MouseEvent.MOUSE_RELEASED && keyFrame != null){
-			if(!selectedKeyFrames.contains(keyFrame)){
-			selectedKeyFrames.add(keyFrame);
-			
-			}
-			}
 
-		else{
+	@Override
+	public void doAction(MouseEvent event, LightTrack lightTrack, KeyFrame keyFrame, Set<KeyFrame> keyFramesSelected,
+			int timestamp, double normalizedY) {
+		// Store selected key frame/s and light track
+		selectedKeyFrames = keyFramesSelected;
+		selectedKeyFrame = keyFrame;
+		selectedLightTrack = lightTrack;
+
+		if (event.getEventType() == MouseEvent.MOUSE_RELEASED && keyFrame != null) {
+			if (!selectedKeyFrames.contains(keyFrame)) {
+				selectedKeyFrames.add(keyFrame);
+
+			}
+		}
+
+		else {
 			selectedKeyFrames.remove(keyFrame);
 		}
-			
-		
-		
-		if (event.getEventType() == MouseEvent.MOUSE_RELEASED && keyFrame == null){
-			if(!selectedKeyFrames.isEmpty()){
+
+		if (event.getEventType() == MouseEvent.MOUSE_RELEASED && keyFrame == null) {
+			if (!selectedKeyFrames.isEmpty()) {
 				return;
-			}else{
+			} else {
 				selectedKeyFrames.clear();
 			}
-			
+
 		}
 
 		if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-			
-			
-			if(keyFrame == null){
+
+			if (keyFrame == null) {
 				return;
 			}
-            withoutSelectedKeyFrame = new HashSet<KeyFrame>(selectedKeyFrames);
-            withoutSelectedKeyFrame.remove(selectedKeyFrame);
-           
+			withoutSelectedKeyFrame = new HashSet<KeyFrame>(selectedKeyFrames);
+			withoutSelectedKeyFrame.remove(selectedKeyFrame);
+
 			TreeSet<KeyFrame> keyFrames = lightTrack.getKeyFrames();
 
 			// Determine lower timestamp limit
@@ -100,29 +94,33 @@ public class SelectTool extends Tool {
 
 			// Determine upper timestamp limit
 			KeyFrame higher = keyFrames.higher(keyFrame);
-			ceiling = higher == null ? Integer.MAX_VALUE : higher.getTimestamp() - HueStew.getInstance().getTickDuration();
-            }
-	
-            else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED && selectedKeyFrame != null) {
-            	//Save previous timestamp/brightness
-            	int tempStamp = selectedKeyFrame.getTimestamp();
-            	LightState tempState = selectedKeyFrame.getState();
-            	int tempBrightness = tempState.getBrightness();
+			ceiling = higher == null ? Integer.MAX_VALUE
+					: higher.getTimestamp() - HueStew.getInstance().getTickDuration();
+		}
+
+		else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED && selectedKeyFrame != null) {
+			// Save previous timestamp/brightness
+			int tempStamp = selectedKeyFrame.getTimestamp();
+			LightState tempState = selectedKeyFrame.getState();
+			int tempBrightness = tempState.getBrightness();
 
 			if (moveHorizontally) {
 				// Update timestamp
-				
+
 				selectedKeyFrame.setTimestamp(timestamp);
-				
-				for (KeyFrame keyframe : withoutSelectedKeyFrame){
-			
-				   keyframe.setTimestamp(keyframe.getTimestamp() + (selectedKeyFrame.getTimestamp() - tempStamp));
-                   //keyframe.setTimestamp(Math.max(keyframe.findLeftmostTimestamp(), Math.min(keyframe.findRightmostTimestamp(), keyframe.getTimestamp() + (selectedKeyFrame.getTimestamp() - tempStamp))));
+
+				for (KeyFrame keyframe : withoutSelectedKeyFrame) {
+
+					keyframe.setTimestamp(keyframe.getTimestamp() + (selectedKeyFrame.getTimestamp() - tempStamp));
+					// keyframe.setTimestamp(Math.max(keyframe.findLeftmostTimestamp(),
+					// Math.min(keyframe.findRightmostTimestamp(),
+					// keyframe.getTimestamp() +
+					// (selectedKeyFrame.getTimestamp() - tempStamp))));
 
 				}
-				
-                }
-	
+
+			}
+
 			if (moveVertically) {
 				if (lightTrack != selectedLightTrack) {
 					// Correct the normalized Y value
@@ -133,26 +131,25 @@ public class SelectTool extends Tool {
 						normalizedY = 1;
 					}
 				}
-				
-				if(selectedKeyFrame == null){
+
+				if (selectedKeyFrame == null) {
 					return;
 				}
-				
+
 				// Update brightness
 				LightState state = selectedKeyFrame.getState();
-            	state.setBrightness((int) (255 * normalizedY));
+				state.setBrightness((int) (255 * normalizedY));
 				selectedKeyFrame.setState(state);
-				
-				for (KeyFrame keyframe : withoutSelectedKeyFrame){
+
+				for (KeyFrame keyframe : withoutSelectedKeyFrame) {
 					LightState stateToSet = keyframe.getState();
 					stateToSet.setBrightness(stateToSet.getBrightness() + (state.getBrightness() - tempBrightness));
 				}
-				
-			}
-            }
 
 			}
-            
+		}
+
+	}
 
 	@Override
 	public void doAction(KeyEvent event) {
@@ -182,5 +179,4 @@ public class SelectTool extends Tool {
 			return Cursor.DEFAULT;
 	}
 
-	}
-	
+}
